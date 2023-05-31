01c43f50ced9bdd624cf9e9915a18072b35945a2
chirayu1901 <chirayu1901@hotmail.com>
2023-05-31 15:55:32 -0400

    Added parser skeleton; Added expression, statment and identifier

---
+++ ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import (
+	"github.com/chirayu1901/monke/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Every valid Monkey program is a series of statements
+// The Program Node is going to be the root node of every AST our parser produces
+type Program struct {
+	Statements []Statement // This is our source code
+}
+
+// Implementing this function means that Program now implements Node interface
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET tokeb
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {return i.Token.Literal}
